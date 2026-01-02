c84880a2b2c93d097a6e93dc0b94b97e4605ae0b
Brady Lor <lor528152@outlook.com>
2026-01-01 18:08:27 -0600

    fixed next.js log errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./ui/navbar";
-import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,16 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="fixed inset-0 -z-10 h-full w-full">
-          <Image
-            src={"/"}
-            alt="background image"
-            fill
-            className="object-cover z-0"
-            quality={100}
-            priority
-          />
-        </div>
         <NavBar></NavBar>
         {children}
       </body>
